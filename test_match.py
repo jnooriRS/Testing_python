846514c3d51ffcbd4dc24db120757c2f1fa835d9
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-25 15:12:15 +0100

    annotations

---
+++ test_match.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def myfunc():
+    raise ValueError("Exception 122 raised")
+
+
+def test_match():
+    with pytest.raises(ValueError, match="123"):
+        myfunc()
+
+
+# Using regex as a comparrison
+# def test_match():
+#     with pytest.raises(ValueError, match=r".* 123 .*"):
+#         myfunc()
